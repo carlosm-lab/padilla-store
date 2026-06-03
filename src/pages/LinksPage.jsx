008574e3e3aab19001f2ab7e53edf15abd8a02c4
testpublic-lab <carlosmolina.public@gmail.com>
2026-06-02 22:45:53 -0600

    Fix icons not loading: Move FontAwesome to global index.html

--- src/pages/LinksPage.jsx
+++ src/pages/LinksPage.jsx
@@ -37,8 +37,6 @@ const LinksPage = () => {
     <>
       <Helmet>
         <title>{SITE_NAME} | Links</title>
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
         <style>{`
           .links-page-container {
             font-family: 'Plus Jakarta Sans', sans-serif;
